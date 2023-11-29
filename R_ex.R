423a2b0eb09da3f66803ff31d5ce143e1199a0e8
iristsai1013 <blue271628@gmail.com>
2023-11-29 17:03:01 +0800

    將四種圖匯總在一起

--- R_ex.R
+++ R_ex.R
@@ -5,3 +5,10 @@ hist(dat[,"eGFR"])
 boxplot(dat[,"eGFR"])
 pie(table(dat[,"Education"]))
 barplot(table(dat[,"Education"]))
+
+
+par(mfrow = c(2, 2))
+hist(dat[,"eGFR"], col = "red")
+boxplot(dat[,"eGFR"], col = "blue")
+pie(table(dat[,"Education"]), col = c("blue", "red", "green"))
+barplot(table(dat[,"Education"]), col = c("gray90", "gray50", "gray10"))
